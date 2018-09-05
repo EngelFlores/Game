7ec6f01e46092b905c0b3c8cbc4caa6c756a4a42
EngelFlores <engel.tads@gmail.com>
2018-09-05 18:34:41 -0300

    Implementa carta desabilitada

--- js/game.js
+++ js/game.js
@@ -1,7 +1,47 @@
 let card = document.getElementsByClassName("contentcard__cards");
-function girar() {
-   this.classList.toggle("card--flip");
+let firstCard = null
+let secondCard = null
+
+function disable() {
+    if (firstCard != null && secondCard != null) {
+        if (firstCard.dataset.card == secondCard.dataset.card) {
+            firstCard.removeEventListener("click", flip);
+            firstCard.classList.add("contentcard__cards--disabled")
+            secondCard.removeEventListener("click", flip);
+            secondCard.classList.add("contentcard__cards--disabled")
+            clearSelectedCards();
+        } else {
+            setTimeout(unflip, 1000);
+        }
+    }
+
 }
+
+function clearSelectedCards() {
+    firstCard = null;
+    secondCard = null;
+}
+
+function flip() {
+    if (firstCard == null) {
+        firstCard = this;
+    } else {
+        if (this == firstCard) {
+            return
+        }
+        secondCard = this;
+    }
+    this.classList.toggle("card--flip");
+    disable();
+
+}
+
+function unflip() {
+    firstCard.classList.toggle("card--flip");
+    secondCard.classList.toggle("card--flip");
+    clearSelectedCards();
+}
+
 for (let i = 0; i < card.length; i++) {
-card[i].addEventListener( "click", girar);
+    card[i].addEventListener("click", flip);
 }
